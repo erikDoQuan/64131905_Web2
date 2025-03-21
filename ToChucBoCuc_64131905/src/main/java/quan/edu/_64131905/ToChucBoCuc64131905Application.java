921bcb32eace77de237c11d1ec594ef70649d8e2
doquan <quan.dcm.64cntt@ntu.edu.vn>
2025-03-21 13:00:42 +0700

    Tochucbocuc

---
+++ ToChucBoCuc_64131905/src/main/java/quan/edu/_64131905/ToChucBoCuc64131905Application.java
@@ -0,0 +1,14 @@
+package quan.edu._64131905;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToChucBoCuc64131905Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToChucBoCuc64131905Application.class, args);
+	}
+
+}
---
+++ ToChucBoCuc_64131905/src/test/java/quan/edu/_64131905/ToChucBoCuc64131905ApplicationTests.java
@@ -0,0 +1,13 @@
+package quan.edu._64131905;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToChucBoCuc64131905ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
