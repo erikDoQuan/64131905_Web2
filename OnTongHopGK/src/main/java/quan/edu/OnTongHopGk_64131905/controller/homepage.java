d3288f4339498ef82e4e86214330bfceb8dc8baf
doquan <quan.dcm.64cntt@ntu.edu.vn>
2025-03-25 09:16:39 +0700

    firt commit

---
+++ OnTongHopGK/src/main/java/quan/edu/OnTongHopGk_64131905/controller/homepage.java
@@ -0,0 +1,5 @@
+package quan.edu.OnTongHopGk_64131905.controller;
+
+public class homepage {
+
+}
---
+++ OnTongHopGK/src/test/java/quan/edu/OnTongHopGk_64131905/OnTongHopGkApplicationTests.java
@@ -0,0 +1,13 @@
+package quan.edu.OnTongHopGk_64131905;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnTongHopGkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
