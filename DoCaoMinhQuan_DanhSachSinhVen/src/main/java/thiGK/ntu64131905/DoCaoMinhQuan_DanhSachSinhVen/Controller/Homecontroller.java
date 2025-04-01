7402c3e3c5153d4810061ceb614e89b544e0ad4f
doquan <quan.dcm.64cntt@ntu.edu.vn>
2025-04-01 09:43:18 +0700

    push lần 2

--- DoCaoMinhQuan_DanhSachSinhVen/src/main/java/thiGK/ntu64131905/DoCaoMinhQuan_DanhSachSinhVen/Controller/Homecontroller.java
+++ DoCaoMinhQuan_DanhSachSinhVen/src/main/java/thiGK/ntu64131905/DoCaoMinhQuan_DanhSachSinhVen/Controller/Homecontroller.java
@@ -1,5 +1,12 @@
 package thiGK.ntu64131905.DoCaoMinhQuan_DanhSachSinhVen.Controller;
 
-public class Homecontroller {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class Homecontroller {
+	 @GetMapping
+	    public String home() {
+	        return "layoutadmin"; 
+	    }
 }
