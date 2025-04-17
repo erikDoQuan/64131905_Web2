c33fa342e8113ed75513eeaa09224486ac0a6607
doquan <quan.dcm.64cntt@ntu.edu.vn>
2025-04-17 08:33:00 +0700

    first commit

---
+++ ThiGK-Lan2/src/main/java/quan/edu/ThiGK_Lan2_64131905/ThiGkLan2Application.java
@@ -0,0 +1,13 @@
+package quan.edu.ThiGK_Lan2_64131905;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThiGkLan2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThiGkLan2Application.class, args);
+	}
+
+}
---
+++ ThiGK-Lan2/src/test/java/quan/edu/ThiGK_Lan2_64131905/ThiGkLan2ApplicationTests.java
@@ -0,0 +1,13 @@
+package quan.edu.ThiGK_Lan2_64131905;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThiGkLan2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
