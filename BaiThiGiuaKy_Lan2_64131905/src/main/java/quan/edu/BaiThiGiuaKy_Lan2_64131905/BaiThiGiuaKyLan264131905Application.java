f05370681fdc997b240279990c6c116a2e485e49
doquan <quan.dcm.64cntt@ntu.edu.vn>
2025-04-18 10:03:08 +0700

    first commit

---
+++ BaiThiGiuaKy_Lan2_64131905/src/main/java/quan/edu/BaiThiGiuaKy_Lan2_64131905/BaiThiGiuaKyLan264131905Application.java
@@ -0,0 +1,13 @@
+package quan.edu.BaiThiGiuaKy_Lan2_64131905;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaiThiGiuaKyLan264131905Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaiThiGiuaKyLan264131905Application.class, args);
+	}
+
+}
---
+++ BaiThiGiuaKy_Lan2_64131905/src/test/java/quan/edu/BaiThiGiuaKy_Lan2_64131905/BaiThiGiuaKyLan264131905ApplicationTests.java
@@ -0,0 +1,13 @@
+package quan.edu.BaiThiGiuaKy_Lan2_64131905;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaiThiGiuaKyLan264131905ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
